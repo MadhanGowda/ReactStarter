94bd25786dfc2806c93ff0a911f750909985d347
Panther <marudhupandiyang@gmail.com>
2017-11-17 18:27:07 +0530

    add swith, not found component

---
+++ src/NotFoundComponent.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const NotFoundComponent = () => (<div>Not Found</div>);
+
+export default NotFoundComponent;
---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
+
+
+import LandingScreenContainer from './Containers/LandingScreenContainer';
+import DiscriptionScreenContainer from './Containers/DiscriptionScreenContainer';
+import NotFoundComponent from './NotFoundComponent';
+
+
+const routes = () => (
+<Router>
+  <div>
+    <Switch>
+        <Route exact path="/" component={LandingScreenContainer}/>
+        <Route exact path="/details" component={DiscriptionScreenContainer}/>
+        <Route path="*" component={NotFoundComponent} />
+    </Switch>
+  </div>
+</Router>);
+
+export default routes;
