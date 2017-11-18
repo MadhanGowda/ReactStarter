8263bcf343982cc02bd377e87b4f828c1d037d22
MADHAN GOWDA <madhan.gowda@gmail.com.com>
2017-11-18 21:40:22 +0530

    discription html part only

--- src/Components/ProductDiscription.js
+++ src/Components/ProductDiscription.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 class ProductDiscription extends React.Component{
 	render(){
+		const product = this.props.produc;
 		return(
-			<div>
-				
+			<div className='productdetail'>
+			<img src={product.url}/>
+					<div className='delatilbox'>	
+	    			<div className="title">{product.name}</div>
+	    				    			<div className="price">Description</div>
+	    			<div className="discription">{product.description}</div>
+
+	    			<div className="price">Rs/- {product.price}</div>
+	    			<div className="quantity-ct">Quantity{'  '}
+	    			<button className="btonp"> 
+	    				<i className="fa fa-plus"></i>{'   '}
+	    				</button>
+	    				<button className="btonm">
+	    				<i className="fa fa-minus"></i>
+	    				</button>
+	    			</div>
+				</div>
 			</div>
 			);
 	}
--- src/Containers/DiscriptionScreenContainer.js
+++ src/Containers/DiscriptionScreenContainer.js
@@ -2,13 +2,45 @@ import React from 'react';
 
 import HeaderBar from '../Components/HeaderBar.js';
 import ProductDiscription from '../Components/ProductDiscription.js';
+import { ProductData } from '../stub';
+import ProductDetails from '../Components/Product';
+import ProductListComponent from '../Components/ProductListComponent.js';
 
+const product={
+        "id": 3,
+        "name": "Very emollient body lotion coconut rescue",
+        "slug": "very-emollient-body-lotion-coconut-rescue-32oz",
+        "price": 0.0,
+        "related_products": [
+            {
+        "id": 1,
+        "name": "body in the buff revitalizing scrub",
+        "slug": "body-in-the-buff-revitalizing-scrub-grapefruit-walnut-shell-9oz",
+        "price": 0.0,
+        "description": "To revitalize dull dry skin, this refreshingly fragrant formula first lathers you up then scrubs you down. The biodegradable buffing beads in this hypo-allergenic scrub contain real ground walnut shells. Warning: may cause exceedingly soft, smooth skin.",
+        "url": "//hain-albabotanica-services-qa-01.galepartners.com//spree/products/131/original/AL00924_9oz_BITB_Grapefruit_Revializing_Scrub_402x.png?1481793358"
+    },
+    {
+        "id": 2,
+        "name": "body in the buff firming scrub",
+        "slug": "body-in-the-buff-firming-scrub-espresso-coffee-bean-9oz",
+        "price": 0.0,
+        "description": "Ground espresso beans exfoliate away impurities while caffeine, coffee seed oil & green coffee bean extract help firm, tighten and tone skin texture.",
+    	"url": "//hain-albabotanica-services-qa-01.galepartners.com//spree/products/131/original/AL00924_9oz_BITB_Grapefruit_Revializing_Scrub_402x.png?1481793358"
+    },
+        ],
+        "description": "When your skin gets to the point that it irritates you at every touch, it’s time to smooth on the big kahuna and show discomfort the door. This rich, creamy lotion made with unrefined coconut oil, coconut milk and aloe vera instantly provides potent moisture relief, a hearty dose of antioxidants and a jolt of caffeine-fueled rejuvenation. Intensely hydrating and hypo-allergenic, it’s exactly the rebound relationship your skin needs.",
+    	"url": "//hain-albabotanica-services-qa-01.galepartners.com//spree/products/131/original/AL00924_9oz_BITB_Grapefruit_Revializing_Scrub_402x.png?1481793358"
+    };
 class DiscriptionScreen extends React.Component{
 	render(){
+		//const item=ProductData;
 		return(
 			<div>
 			<HeaderBar />
-			<ProductDiscription />
+			<ProductDiscription produc={product} key={product.id}/>
+			<div className="rp">Related Products</div>
+			<ProductListComponent prod = {product.related_products} />
 			</div>
 			);
 	}
