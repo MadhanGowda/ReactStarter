daf9b402d4a87e5b53c16ca40474c73701c78b2c
MADHAN GOWDA <madhan.gowda@gmail.com.com>
2017-11-21 16:56:57 +0530

    adding to db

--- src/Actions/cart.js
+++ src/Actions/cart.js
@@ -1,12 +1,27 @@
 import { ADD_TO_CART } from './actionTypes.js';
 import { REMOVE_FROM_CART } from './actionTypes.js';
+import axios from 'axios';
 
 
 
 export function addToCart(product){
+	axios.post('http://127.0.0.1:8000/count/', {
+	    id: product.id,
+	    count: 1,
+	  })
+	  .then(function (response) {
+	    console.log(response);
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  });
+	  //debugger;
 	return {type: ADD_TO_CART , data: product};
 }
 
 export function removeFromCart(product){
 	return {type: REMOVE_FROM_CART , data: product};
 }
+
+
+// function addCartRequest(product)
